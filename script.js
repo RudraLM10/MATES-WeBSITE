558a0e7ea42d8888d5b486d3a97c517ef9b49d36
RudraLM10 <rudralm10@gmail.com>
2023-11-25 00:36:43 +0530

    pre-loader

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const openMenu = document.querySelector(".openmenu");
 const closeMenu = document.querySelector(".closemenu");
 const fullImgBox=document.getElementById('fullImgBox')
 const fullImg=document.getElementById('fullImg')
+const preloader = document.getElementById("loading");
+
+function myFunction() {
+  setTimeout(function(){ preloader.style.display = "none"; }, 2000);
+}
 
 openMenu.addEventListener("click", function () {
   navLinks.style.right = "0";
