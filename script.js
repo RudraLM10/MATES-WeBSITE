ea199e9f14b83603b15fc9e3a132e450f4d548f9
RudraLM10 <rudralm10@gmail.com>
2023-11-24 19:17:48 +0530

    photo-gallery

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const navLinks = document.getElementById("navLinks");
 const openMenu = document.querySelector(".openmenu");
 const closeMenu = document.querySelector(".closemenu");
+const fullImgBox=document.getElementById('fullImgBox')
+const fullImg=document.getElementById('fullImg')
 
 openMenu.addEventListener("click", function () {
   navLinks.style.right = "0";
@@ -11,3 +13,12 @@ openMenu.addEventListener("click", function () {
 closeMenu.addEventListener("click", function () {
   navLinks.style.right = "-100%";
 });
+
+function openFullImg(pic){
+  fullImgBox.style.display='flex'
+  fullImg.src=pic;
+}
+
+function closeFullImg(){
+  fullImgBox.style.display='none'
+}
